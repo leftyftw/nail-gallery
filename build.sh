ee68715ae558b047560f39d323e545b3353173cc
leftyftw <mr.jaime.torres@gmail.com>
2023-06-06 22:37:56 -0400

    Fix host

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -t leftyftw/nail-gallery .
+# docker build -t leftyftw/nail-gallery:2.0 .
+docker buildx build --platform linux/amd64 --push -t leftyftw/nail-gallery:2.0 .
--- code/src/index.js
+++ code/src/index.js
@@ -51,7 +51,7 @@ fastify.get('/nails/collections/pack/:sitename', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({port: 3000})
+    await fastify.listen({port: 3000, host: '0.0.0.0' })
     fastify.log.info(`Server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
