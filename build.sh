0ec38f296858e935ad973e0f727ff1f94a1e22e6
leftyftw <mr.jaime.torres@gmail.com>
2023-12-27 21:13:34 -0500

    JPEG is not a video

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # docker build -t leftyftw/nail-gallery:2.0 .
-docker buildx build --platform linux/amd64 --push -t leftyftw/nail-gallery:2.0 .
+docker buildx build --platform linux/amd64 --push -t leftyftw/nail-gallery:latest -t leftyftw/nail-gallery:2.1 .
